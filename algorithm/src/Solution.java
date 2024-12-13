775c1f6be84b57316f75564ac68d9d3ce216e380
playhuck <wngur89@gmail.com>
2024-12-13 17:00:38 +0900

    solve : stack 234 palindrome liked list

--- algorithm/src/Solution.java
+++ algorithm/src/Solution.java
@@ -1,5 +1,4 @@
 import javax.swing.tree.TreeNode;
-import java.awt.List;
 import java.math.BigInteger;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -31,38 +30,6 @@ public class Solution {
 //        System.out.println(captureForts(
 //                new int[]{-1,-1,0,1,0,0,1,-1,1,0}
 //        ));
-    }
-
-    /*
-        단일 연결 리스트의 head가 주어질 때, 이것이 *회문(palindrome)*이면 true를, 그렇지 않으면 false를 반환하세요.
-        [참고: 회문이란 앞에서부터 읽으나 뒤에서부터 읽으나 같은 값이 되는 것을 말합니다. 예를 들어 1->2->2->1은 회문입니다.]
-     */
-    public boolean isPalindrome(ListNode head) {
-
-        Stack<Integer> stack = new Stack<>();
-
-        ListNode cur = head;
-
-        while (cur.next != null) {
-            stack.push(cur.val);
-            cur = cur.next;
-        }
-
-        stack.push(cur.val);
-
-        cur = head;
-
-        while (cur.next != null) {
-
-            int stackLastVal = stack.peek();
-            if(Objects.equals(stackLastVal, cur.val)) {
-                stack.pop();
-            }
-
-            cur = cur.next;
-        }
-
-        return stack.isEmpty();
     }
 
      public class ListNode {
