a2a133ceb595aae5cb65d80f3699d34405152520
playhuck <wngur89@gmail.com>
2024-12-16 16:10:36 +0900

    solve : Stack589, 590 N Tree

--- algorithm/src/stack_queue/Stack589_NaryTreePreorderTraversal.java
+++ algorithm/src/stack_queue/Stack589_NaryTreePreorderTraversal.java
@@ -11,6 +11,11 @@ public class Stack589_NaryTreePreorderTraversal {
        N진 트리의 입력 직렬화는 레벨 순서 순회로 표현됩니다.
        각 자식 그룹은 null 값으로 구분됩니다 (예시 참조)
     */
+    /*
+        n진 트리의 root가 주어질 때, 해당 노드들의 값을 전위 순회한 결과를 반환하세요.
+        N진 트리의 입력 직렬화는 레벨 순서 순회로 표현됩니다.
+        각 자식 그룹은 null 값으로 구분됩니다 (예시 참조)
+     */
     public List<Integer> preorder(Node root) {
 
         List<Integer> preOrder = new ArrayList<>();
@@ -29,24 +34,12 @@ public class Stack589_NaryTreePreorderTraversal {
 
         while (!root.children.isEmpty()) {
 
-            /*
-                1. 리스트가 빈 값이 아닐 때, 아래가 존재한다면 아래로 내려갑니다.
-                2. 만약 아래가 null이라면 현제 내 배열의 다음값을 선택합니다.
-                3. 다음 값이 존재한다면, 현재 값은 children의 다음 인덱스입니다.
-                4. 다음 값이 없다면 pop하여 올라갑니다.
-                5. 만약 배열이 비어있다면 pop하여 올라갑니다.
-             */
-
             if (curr.val != Integer.MIN_VALUE) {
-                System.out.println("CURR VAL : " + curr.val);
                 preOrder.add(curr.val);
                 curr.val = Integer.MIN_VALUE;
             }
 
-            System.out.println("CURR CHILDREN : " + curr.children.size());
-            System.out.println("ROOT CHILDREN : " + root.children.size());
-
-            if (!curr.children.isEmpty() && Objects.nonNull(root.children)) {
+            if (!curr.children.isEmpty()) {
 
                 if (Objects.nonNull(curr.children.get(0))) {
                     stacks.push(curr);
--- algorithm/src/stack_queue/Stack590_NaryTreePostOrderTraversal.java
+++ algorithm/src/stack_queue/Stack590_NaryTreePostOrderTraversal.java
@@ -1,37 +1,11 @@
-import javax.swing.tree.TreeNode;
-import java.math.BigInteger;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+package stack_queue;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Stack;
 
-    public static void main(String[] args) {
-
-//        System.out.println(countGoodRectangles(
-//                new int[][]{{5,8},{3,9},{5,12},{16,5}}
-//        ));
-//        System.out.println(new RecentCounter().ping(
-//                new String[]{"RecentCounter", "ping", "ping", "ping", "ping"}
-//        ));
-//        System.out.println(inorderTraversal(
-//                "(()})"
-//        ));
-//        System.out.println(minNumber(
-//                new String[]{"hello","world","leetcode"}, "welldonehoneyr"
-//        ));
-//        System.out.println(countStudents(
-//                new int[]{1,1,1,0,0,1}, new int[]{1,0,0,0,1,1}
-//        ));
-//        System.out.println(timeRequiredToBuy(
-//                new int[]{5,1,1,1}, 0
-//        ));
-//        System.out.println(captureForts(
-//                new int[]{-1,-1,0,1,0,0,1,-1,1,0}
-//        ));
-    }
-
+public class Stack590_NaryTreePostOrderTraversal {
     /*
         n진 트리의 root가 주어질 때, 해당 노드들의 값을 후위 순회한 결과를 반환하세요.
         N진 트리의 입력 직렬화는 레벨 순서 순회로 표현됩니다. 각 자식 그룹은 null 값으로 구분됩니다 (예시 참조)
@@ -92,5 +66,4 @@ public class Solution {
             children = _children;
         }
     }
-
 }
