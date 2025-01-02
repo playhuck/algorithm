803708643c900a20b358dd9845a13146d944f95e
playhuck <wngur89@gmail.com>
2025-01-02 15:43:18 +0900

    solve : Binary Tree

--- algorithm/src/binary_tree/BT112.java
+++ algorithm/src/binary_tree/BT112.java
@@ -1,49 +1,8 @@
-import binary_tree.BT104;
-import stack_queue.Stack144_BinaryTreePreorderTraversal;
-import stack_queue.Stack94_BinaryTreeInOrderTraversal;
+package binary_tree;
 
 import java.util.*;
 
-public class Solution_Company {
-
-    public static void main(String[] args) {
-
-//        System.out.println(countGoodRectangles(
-//                new int[][]{{5,8},{3,9},{5,12},{16,5}}
-//        ));
-//        System.out.println(new RecentCounter().ping(
-//                new String[]{"RecentCounter", "ping", "ping", "ping", "ping"}
-//        ));
-//        System.out.println(simplifyPath(
-//                "/home/../../.."
-//        ));
-//        System.out.println(minNumber(
-//                new String[]{"hello","world","leetcode"}, "welldonehoneyr"
-//        ));
-//        System.out.println(countStudents(
-//                new int[]{1,1,1,0,0,1}, new int[]{1,0,0,0,1,1}
-//        ));
-//        System.out.println(timeRequiredToBuy(
-//                new int[]{5,1,1,1}, 0
-//        ));
-//        System.out.println(Arrays.toString(maxRowSubsequence(
-//                new int[]{0,0,3,3}, 2
-//        )));
-//        System.out.println(findDiagonalOrder(
-//                new int[]{{1,2,3},{4,5,6},{7,8,9}}
-//        ));
-        List<List<Integer>> numsList = Arrays.asList(
-                Arrays.asList(1, 2, 3),
-                List.of(4),
-                List.of(5, 6, 7),
-                List.of(8),
-                Arrays.asList(9, 10, 11)
-        );
-//        System.out.println((smallestChair(new int[][]{{2, 4}, {4, 9}, {3, 4}, {6, 8}, {5, 10}}, 4)));
-        // {{2,4}, {4,9}, {3,4}, {6,8}, {5,10}}
-        // {{3,10}, {1,5}, {2,6}}
-    }
-
+public class BT112 {
     /*
         이진 트리의 root와 정수 targetSum이 주어질 때, 트리에 루트에서 리프까지의 경로가 있어서
         그 경로를 따라 모든 값을 더했을 때 targetSum과 같다면 true를 반환하세요.
---
+++ algorithm/src/binary_tree/RE_BT108.java
@@ -0,0 +1,45 @@
+package binary_tree;
+
+public class RE_BT108 {
+    /*
+        원소들이 오름차순으로 정렬된 정수 배열 nums가 주어질 때, 이를 높이 균형 이진 탐색 트리로 변환하세요.
+        [참고: 높이 균형 이진 트리란 모든 노드의 왼쪽과 오른쪽 하위 트리의 높이 차이가 1 이하인 이진 트리를 말합니다.]
+
+        모든 값들은 중간값을 기준으로 정렬됩니다.
+     */
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+        return recursion(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode recursion(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = (left + right) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = recursion(nums, left, mid - 1);
+        node.right = recursion(nums, mid + 1, right);
+        return node;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
