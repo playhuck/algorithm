4193288dc17a0dfbaba53afefd863daa6581a2bb
kimjuhyuk2 <wngur89@imantisco.com>
2025-01-18 19:14:20 +0900

    solve : BFS637

--- algorithm/src/bfs/BFS637.java
+++ algorithm/src/bfs/BFS637.java
@@ -1,44 +1,8 @@
-import com.sun.source.tree.Tree;
-import stack_queue.Stack144_BinaryTreePreorderTraversal;
+package bfs;
 
-import javax.swing.tree.TreeNode;
-import java.math.BigInteger;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
-public class Solution {
-
-    public static void main(String[] args) {
-
-//        System.out.println(countGoodRectangles(
-//                new int[][]{{5,8},{3,9},{5,12},{16,5}}
-//        ));
-//        System.out.println(new RecentCounter().ping(
-//                new String[]{"RecentCounter", "ping", "ping", "ping", "ping"}
-//        ));
-//        System.out.println(simplifyPath(
-//                "/home/../../.."
-//        ));
-//        System.out.println(minNumber(
-//                new String[]{"hello","world","leetcode"}, "welldonehoneyr"
-//        ));
-//        System.out.println(countStudents(
-//                new int[]{1,1,1,0,0,1}, new int[]{1,0,0,0,1,1}
-//        ));
-//        System.out.println(timeRequiredToBuy(
-//                new int[]{5,1,1,1}, 0
-//        ));
-        System.out.println(splitPainting(
-                new int[][]{
-                        {1, 4, 5},
-                        {4, 7, 7},
-                        {1, 7, 9}
-                }
-        ));
-    }
-
+public class BFS637 {
     /*
         이진 트리의 root가 주어질 때, 각 레벨에 있는 노드들의 평균값을 배열 형태로 반환하세요.
         실제 답과 10-5 이내의 오차가 있는 답은 허용됩니다.
@@ -102,5 +66,4 @@ public class Solution {
             this.right = right;
         }
     }
-
 }
